e0f4bc1a5bb5e2444afad9a436dc28cabf957b88
choymoon <choymoonk@gmail.com>
2019-07-24 17:39:02 +0900

    Initial Setting And Color Setting

---
+++ INU Shuttle/UIColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIColorExtention.swift
+//  INU Shuttle
+//
+//  Created by Cho on 24/07/2019.
+//  Copyright © 2019 Cho. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let mainBlue: UIColor = UIColor.init(red: 0, green: 97 / 255, blue: 244 / 255, alpha: 1.0)
+    
+    static let mainGrey: UIColor = UIColor.init(red: 96 / 255, green: 96 / 255, blue: 96 / 255, alpha: 1.0)
+    
+    static let mainRed: UIColor = UIColor.init(red: 216 / 255, green: 0, blue: 0, alpha: 1.0)
+    
+}
